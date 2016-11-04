7a62848312525f97e894132cdc202454b1fb1bc7
pyknic <emil@speedment.com>
2016-11-04 14:52:40 -0700

    Play nicely with Tomcat

--- src/main/java/com/extspeeder/example/financialdemo/MainApp.java
+++ src/main/java/com/extspeeder/example/financialdemo/MainApp.java
@@ -2,9 +2,16 @@ package com.extspeeder.example.financialdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApp {
+public class MainApp extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApp.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApp.class, args);
